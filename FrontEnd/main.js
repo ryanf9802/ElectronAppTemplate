a2d751d07e600faa331157293518cdee3fdfe7c3
Ryan Fitzpatrick <ryanf9802@gmail.com>
2024-11-07 12:14:39 -0500

    base

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,29 +1,29 @@
-import { app, BrowserWindow } from "electron";
-import { join } from "path";
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
 
-  win.loadFile("public/index.html");
+  win.loadFile('public/index.html');
 };
 
 app.whenReady().then(createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
--- FrontEnd/webpack.config.js
+++ FrontEnd/webpack.config.js
@@ -1,29 +1,33 @@
-import { resolve as _resolve, join } from "path";
+const path = require('path');
 
-export const entry = "./src/index.js";
-export const output = {
-  path: _resolve(__dirname, "dist"),
-  filename: "bundle.js",
-};
-export const module = {
-  rules: [
-    {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
       },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-  ],
-};
-export const devServer = {
-  contentBase: join(__dirname, "dist"),
-  compress: true,
-  port: 3000,
-};
-export const resolve = {
-  extensions: [".js", ".jsx"],
+    compress: true,
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
